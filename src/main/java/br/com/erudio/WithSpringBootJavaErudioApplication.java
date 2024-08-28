bb7156fb2f2e222a0b4d16281abbf10f2e5dd047
ulisses953 <ulisseskranzdamotta@gmail.com>
2024-08-28 10:25:38 -0300

    inicio do desenvolvimento

---
+++ src/main/java/br/com/erudio/WithSpringBootJavaErudioApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithSpringBootJavaErudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithSpringBootJavaErudioApplication.class, args);
+	}
+
+}
